29e6fbe487b9c2fbbfb9fdfb003c8e5ce05c3d17
lanfurama <lanfurama@example.com>
2026-01-13 13:46:15 +0700

    update

--- constants.ts
+++ constants.ts
@@ -18,7 +18,7 @@ export const GROUPS: Group[] = [
   { id: 4, name: "Group 4", leader: "", memberCount: 7, slideUrl: "https://docs.google.com/presentation/d/1sbawEyNGy2RdE2CoBRyaJl1n0Tl9j-aitG2B5Fj3Nzs/edit?usp=sharing", color: "#166534" }, // Xanh lá cây đậm
   { id: 5, name: "Group 5", leader: "", memberCount: 7, slideUrl: "https://docs.google.com/presentation/d/1u44dpw7Siw--2U95G5ocMI35krdswlwATJO5Trv3OX8/edit?usp=sharing", color: "#8B5CF6" }, // Tím
   { id: 6, name: "Group 6", leader: "", memberCount: 7, slideUrl: "https://docs.google.com/presentation/d/1kztKo0B154OML5QErhahAPYGW6lbUDg7XQTzvgt32Go/edit?usp=sharing", color: "#38BDF8" }, // Xanh nước biển nhạt
-  { id: 7, name: "Group 7", leader: "", memberCount: 7, slideUrl: "https://docs.google.com/presentation/d/1kztKo0B154OML5QErhahAPYGW6lbUDg7XQTzvgt32Go/edit?usp=sharing", color: "#0891B2" }, // Cyan
+  { id: 7, name: "Group 7", leader: "", memberCount: 7, slideUrl: "https://docs.google.com/presentation/d/1hshmMJrpDMHDL4F4_2WQ5E91xSEhrZC_0deb0t9q3Ug/edit?usp=sharing", color: "#0891B2" }, // Cyan
 ];
 
 // 3. MOCK DATA FOR DASHBOARD VISUALIZATION
